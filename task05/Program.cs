be16175e5659810ef0101c0cc8a5d96306bd68d5
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 18:04:36 +0900

    result is looks as like one string

--- task05/Program.cs
+++ task05/Program.cs
@@ -5,3 +5,9 @@
 2 -> -2, -1, 0, 1, 2
 */
 
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = -number; i <= number; i++)
+{
+    Console.Write(i+ " ");
+}
