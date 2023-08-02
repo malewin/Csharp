149b0b78c3efd4527824c1434d530f7a07742f5a
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 14:59:00 +0900

    example2 is solved

--- task01/Program.cs
+++ task01/Program.cs
@@ -10,3 +10,12 @@ a = -3; b = 9 -> нет
 Console.WriteLine("Введите два числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+ {
+    Console.WriteLine("является");
+ }
+ else
+ {
+    Console.WriteLine("не является");
+ }
