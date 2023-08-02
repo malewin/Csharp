f6bb000c619edf073a0acf8a87657af2ac669189
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 16:46:02 +0900

    finished

--- task01/Program.cs
+++ task01/Program.cs
@@ -12,10 +12,10 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1 == number2 * number2)
- {
+{
     Console.WriteLine("является");
- }
- else
- {
+}
+else
+{
     Console.WriteLine("не является");
- }
+}
