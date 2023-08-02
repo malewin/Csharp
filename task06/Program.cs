d86dcdc91b47902c41c522b11103c0dbe4c1faf5
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 23:51:05 +0900

    task6 is exellent completed

--- task06/Program.cs
+++ task06/Program.cs
@@ -4,15 +4,27 @@
 4 -> да
 -3 -> нет
 7 -> нет 
+Дополнительно: + выдаёт информацию если число ещё и отрицательное.
 */
 
 Console.WriteLine ("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number%2 == 0)
+if(number%2 == 0 && number >= 0)
 {
   Console.WriteLine ("Число чётное!"); 
 }
 else
+if(number%2 == 0 && number < 0)
+{
+  Console.WriteLine ("Число чётное! и отрицательное"); 
+}
+else
+if(number > 0)
 {
    Console.WriteLine ("Число нечётное... :( ");  
+}
+else
+if(number < 0)
+{
+   Console.WriteLine ("Число нечётное и отрицательное");  
 }
