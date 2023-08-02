d52e19d01200b8c032905a4ec531c4922e50cb9f
malewin <139165145+malewin@users.noreply.github.com>
2023-08-03 00:16:58 +0900

    task8 is exellent completed

---
+++ task08/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число: N= ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number <= 1)
+{
+    Console.WriteLine("Чётных чисел, удовлетворяющих условию задачи, - нет");
+}
+else
+Console.WriteLine("Числа в диапазоне от 1 до вашего числа N: ");
+for (int i = 2; i <= number; i=i+2)
+{
+    Console.Write(i+ ", "); 
+}
