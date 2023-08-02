b7655291adc190b5c0c554fc661e4d5dc6f051d0
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 22:06:42 +0900

    Example7 is solved

--- task07/Program.cs
+++ task07/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите програму, котоаря принимает на вход трёхзначное число
+и на выходе показывает по следнюю цифру этого числа */
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Последняя цифра: "+ number%10);
+
+
