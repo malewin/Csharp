83f562a07ca963896e597fb21058ce428912dcf3
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 23:25:19 +0900

    task4 is exelent completed

--- task04/Program.cs
+++ task04/Program.cs
@@ -13,20 +13,48 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
+int min = number1;
 
-if(number1 > number2 && number1 > number3)
+if(number1 > number2 && number1 > number3 && number2 > number3) // первое больше обоих, второе больше третьего
 {
     Console.WriteLine("Максимальное число: " + max);
+    min = number3;
+    Console.WriteLine("А минимальное число: " + min);
 }
 else
-if(number1 > number2 && number1 < number3)
+if(number1 > number2 && number1 > number3 && number2 < number3) // первое больше обоих, третье больше второго
+{
+    Console.WriteLine("Максимальное число: " + max);
+    min = number2;
+    Console.WriteLine("А минимальное число: " + min);
+}
+else
+if(number1 > number2 && number1 < number3) // третье больше обоих, первое больше второго
 {
     max = number3;
     Console.WriteLine("Максимальное число: " + max);
+    min = number2;
+    Console.WriteLine("А минимальное число: " + min);
 }
 else
-if(number1 < number2 && number1 > number3)
+if(number1 < number2 && number1 < number3 && number3 > number2) // третье больше обоих, второе больше первого
+{
+    max = number3;
+    Console.WriteLine("Максимальное число: " + max);
+    Console.WriteLine("А минимальное число: " + min);
+}
+else
+if(number1 < number2 && number1 > number3) // второе больше обоих, первое больше третьего
 {
     max = number2;
     Console.WriteLine("Максимальное число: " + max);
+    min = number3;
+    Console.WriteLine("А минимальное число: " + min);
+}
+else
+if(number1 < number2 && number1 < number3 && number2 > number3) // второе больше обоих, третье больше первого
+{
+    max = number2;
+    Console.WriteLine("Максимальное число: " + max);
+    Console.WriteLine("А минимальное число: " + min);
 }
