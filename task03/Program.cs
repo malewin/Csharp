536007a2da3cd172b29469d77e01774cd59bcb10
malewin <139165145+malewin@users.noreply.github.com>
2023-08-02 16:32:15 +0900

    Example3 is solved

--- task03/Program.cs
+++ task03/Program.cs
@@ -5,5 +5,44 @@
 5 -> пятница
 */
 
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите цифру дня недели: ");
 
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else
+if(number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else
+if(number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else
+if(number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else
+if(number == 5)
+{
+    Console.WriteLine("Вторник");
+}
+else
+if(number == 6)
+{
+    Console.WriteLine("Вторник");
+}
+else
+if(number == 7)
+{
+    Console.WriteLine("Вторник");
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
