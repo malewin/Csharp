d95bc6960ad9c30008106899f8b378a0af4cc2ad
malewin <139165145+malewin@users.noreply.github.com>
2023-08-07 20:04:43 +0900

    apologize is completed 2

--- MyTask_IamSorry_1000/Program.cs
+++ MyTask_IamSorry_1000/Program.cs
@@ -1,9 +1,9 @@
 ﻿/* я извиняюсь тысячу раз */
 
-string iamsorry;
+string iamsorry = "прости меня,";
 
 for (int i = 0; i < 1000; i++)
 {
-    Console.Write(iamsorry + " ")
+    Console.Write(iamsorry + " ");
 }
 
