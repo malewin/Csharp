c6e28907d90180ab7a81a504676c3ddd24ed9cd9
malewin <139165145+malewin@users.noreply.github.com>
2023-08-09 00:19:33 +0900

    ефыл16 шы ышщдмув

---
+++ task16/Program.cs
@@ -0,0 +1,16 @@
+﻿/* напишите программу,  которая принимает на вход два числа и выдаёт на выход
+является ли одно число квадратом другого*/
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1==number2*number2 || number2==number1*number1)
+{
+    Console.WriteLine("Одно число - квадрат другого");
+}
+else 
+{
+    Console.WriteLine("Одно число - не является квадратом другого");
+}
