7e4e0b0e21fa4b5d196873b2acbb06f06be4009b
malewin <139165145+malewin@users.noreply.github.com>
2023-08-09 18:46:33 +0900

    stupid but right solution

---
+++ task15/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+int [] budni = {1,2,3,4,5};
+int [] vyhodnye = {6,7};
+Console.WriteLine("Введите цифру дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == budni[0] || number == budni[1] || number == budni[2] || number == budni[3] || number == budni[4])
+{
+    Console.WriteLine("Это не выходной");
+}
+else if (number == vyhodnye[0] || number == vyhodnye[1])
+{
+    Console.WriteLine("УРА! - выходной");
+}
+else if (number != budni[0] || number != budni[1] || number != budni[2] || number != budni[3] || number != budni[4] && number != vyhodnye[0] || number != vyhodnye[1])
+{
+    Console.WriteLine("Такого дня недели нет");
+}
