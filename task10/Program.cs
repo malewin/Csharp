ded6df679a83094ed69395afcc13f1a425f2ada2
malewin <139165145+malewin@users.noreply.github.com>
2023-08-09 15:24:39 +0900

    task10 is solved

--- task10/Program.cs
+++ task10/Program.cs
@@ -8,4 +8,19 @@
 
 
 Console.WriteLine("Введите трёхзначное число");
-
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber <1000 && userNumber > 99 || userNumber > -1000 && userNumber < -99)
+{
+    int secondNumber = userNumber / 10 % 10;
+    if (secondNumber < 0)
+    {
+        secondNumber = secondNumber * (-1);
+        Console.WriteLine($"вторая цифра числа = {secondNumber}");
+    }
+    else
+    Console.WriteLine($"вторая цифра числа = {secondNumber}");
+}
+else 
+{
+    Console.WriteLine("число не трёхзначное");
+}
