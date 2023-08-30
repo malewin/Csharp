18f822caa0c36b77506c1cdc19086bd3f61b6d52
malewin <139165145+malewin@users.noreply.github.com>
2023-08-30 22:52:09 +0900

    example is completed

---
+++ task22/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от 1 до N. */
+
+Console.WriteLine("Введите число N");
+
+int numberN = Convert.ToInt32(Console.Readline());
+double result = 0;
+if (numberN < 0)
+{
+    numberN = numberN * (-1);
+}
+for (int i = 1; i <= numberN; i++)
+{
+    result = Math.Pow (i, 2);
+    Console.WriteLine(result);
+}
