2bf3d61059e7290469bbdd6dcc61de58e98e1763
malewin <139165145+malewin@users.noreply.github.com>
2023-08-30 23:45:24 +0900

    example is completed

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N. */
+
+Console.WriteLine("Введите число N");
+
+int numberN = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+if (numberN < 0)
+{
+    numberN = numberN * (-1);
+}
+for (int i = 1; i <= numberN; i++)
+{
+    result = Math.Pow (i, 3);
+    Console.WriteLine(result);
+}
