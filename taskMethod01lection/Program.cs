0139642f9cdf091fb74b3418f9cbfa2fe82ae958
malewin <139165145+malewin@users.noreply.github.com>
2023-08-30 10:24:38 +0900

    first task is writed3

--- taskMethod01lection/Program.cs
+++ taskMethod01lection/Program.cs
@@ -16,11 +16,11 @@ string Replace(string text, char oldValue, char newValue)
 {
     string result = String.Empty;
 
-    int length = text.length;
+    int length = text.Length;
     for (int i = 0; i < length; i++)
     {
         if(text[i] == oldValue) result = result + $"{newValue}";
-        else result + $"{str[i]}";
+        else result = result + $"{str[i]}";
     }
 
     return result;
