53785753624bbcfc145c2d20c14521fba2da237b
malewin <139165145+malewin@users.noreply.github.com>
2023-08-31 00:18:16 +0900

    example is completed

---
+++ task20/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 2-D пространстве */
+
+Console.WriteLine("Input [x, y] coordinates for А-point");
+int xA = Convert.ToInt32(Console.ReadLine());
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" A-point: [{xA},{yA}]");
+
+Console.WriteLine("Input [x, y] coordinates for B-point");
+int xB = Convert.ToInt32(Console.ReadLine());
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" B-point: [{xB},{yB}]");
+
+double diffAB = Math.Sqrt ((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA));
+
+Console.WriteLine($"Different between A-point and B-point: {diffAB}");
