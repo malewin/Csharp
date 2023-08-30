b22987dc3a65dccc72f19b5a04d1b4d40069e001
malewin <139165145+malewin@users.noreply.github.com>
2023-08-30 23:41:19 +0900

    example is completed2

--- task19/Program.cs
+++ task19/Program.cs
@@ -4,7 +4,7 @@
 // 65456 - да
 // 12567 - нет
 
-Console.WriteLine ("Input 5-symbol number: ");
+Console.WriteLine ("Input 5-symbol's number: ");
 
 int userNumber = Convert.ToInt32(Console.ReadLine());
 int mirrorNumber = 0;
@@ -13,9 +13,13 @@ if (userNumber < 0)
 {
     userNumber = userNumber * (-1);
 }
+if (userNumber > 9999 && userNumber < 100000)
+{
 for (int i = userNumber; i > 0; i = i / 10)
 {
-    mirrorNumber = userNumber % 10 + mirrorNumber * 10;
+    mirrorNumber = i % 10 + mirrorNumber * 10;
 }
 if (mirrorNumber == userNumber) {Console.WriteLine($"number {userNumber} is polindrom");}
 else {Console.WriteLine($"number {userNumber} isn't polindrom");}
+}
+else Console.WriteLine($"number {userNumber} isn't 5-symbol's");
