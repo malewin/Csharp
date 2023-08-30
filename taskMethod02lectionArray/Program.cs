b45c3f96853618292d5e1f4259bba44fe9274b32
malewin <139165145+malewin@users.noreply.github.com>
2023-08-30 10:48:28 +0900

    printArrayMethod is added

---
+++ taskMethod02lectionArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
