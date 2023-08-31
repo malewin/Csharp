e843795ce86f8001677e19563756feaf6a6825a2
malewin <139165145+malewin@users.noreply.github.com>
2023-08-31 14:10:06 +0900

    example is completed

---
+++ task26/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+
+456 - 3
+78 - 2
+89126 - 5
+*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+
+Console.WriteLine($"в числе {userNumber} - {Quantity(userNumber)} символов");
