a948d7c8419475ffea917649142115d5cd16348f
malewin <139165145+malewin@users.noreply.github.com>
2023-08-31 20:49:51 +0900

    example is completed

---
+++ task28/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N включительно
+*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int ProizvedenieDoChisla(int number)
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    {
+     count = count * i;
+    }
+    return count;
+}
+
+Console.WriteLine($"произведение чисел от 1 до {userNumber} = {ProizvedenieDoChisla(userNumber)}");
