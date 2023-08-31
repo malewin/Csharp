72149fc98c60c9f1611c0458ed42941463e17144
malewin <139165145+malewin@users.noreply.github.com>
2023-08-31 21:21:46 +0900

    example is completed

---
+++ task30/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, которая выодит массив из 8 элементов,
+заполненный нулями и единицами в случайном порядке
+*/
+/*
+void PrintArray()
+{
+    Console.WriteLine(GetRandomArray);
+}
+*/
+
+int [] GetRandomArray()
+{
+    int [] array1 = new int [8];
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i]= new Random().Next(0,2);
+    }
+    return array1;
+}
+
+int [] userArray = GetRandomArray();
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write($"{arrayToPrint[i]}, ");
+    }
+    Console.Write($"]");
+}
+
+PrintArray(userArray);
