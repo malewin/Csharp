82670c3e597f7625ef47cba58dd586b7667186b9
malewin <139165145+malewin@users.noreply.github.com>
2023-08-31 13:51:12 +0900

    example is completed2

---
+++ task24/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход
+число А и выдаёт сумму чисел от 1 до А */
+
+Console.WriteLine("Введите число А");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int SummOfNumber(int number)
+{
+    int summ = 0;
+    for (int i = 1; i <= userNumber; i++)
+    {
+        summ = summ + i;
+    }
+    return summ;
+}
+
+Console.WriteLine($"сумма цифр до числа {userNumber} включительно равна {SummOfNumber(userNumber)}");
