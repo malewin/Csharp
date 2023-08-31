c26af657444dcc455a962f0ba70ffbaba112ef6f
malewin <139165145+malewin@users.noreply.github.com>
2023-08-31 22:07:08 +0900

    example is completed

---
+++ task25/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (А и В) 
+и возводит число А в натуральную степень В.
+*/
+
+/*
+void EnterAnumbers()
+{
+    Console.WriteLine("введите число А: ");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите число B: ");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+}
+EnterAnumbers(); // хотел ввод сделать через метод тоже, но запутался.
+*/
+
+Console.WriteLine("введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+double AvStepeniB(int numberA, int numberB)
+{
+    if (numberB < 0) { numberB = numberB * (-1);}
+
+    double result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+Console.WriteLine($"число {numberA} в натуральной степени {numberB} равно {AvStepeniB(numberA, numberB)}");
