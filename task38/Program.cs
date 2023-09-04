2fdab7448d61932c91935d4d702bbacf2dd982a8
malewin <139165145+malewin@users.noreply.github.com>
2023-09-04 13:00:19 +0900

    after change int type to double type

--- task38/Program.cs
+++ task38/Program.cs
@@ -6,22 +6,22 @@
 Console.WriteLine("Введите количество эдементов в массиве:");
 int arrayL = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите нижний предел диапазона чисел массива:");
-int minBorder = Convert.ToInt32(Console.ReadLine());
+double minBorder = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите верхний предел диапазона чисел массива:");
-int maxBorder= Convert.ToInt32(Console.ReadLine()) + 1;
+double maxBorder= Convert.ToDouble(Console.ReadLine()) + 1;
 
-int [] GetRandomArray(int arrayL, int minBorder, int maxBorder)
+double [] GetRandomArray(int arrayL, double minBorder, double maxBorder)
 {
-    int [] array1 = new int [arrayL];
+    double [] array1 = new double [arrayL];
     for (int i = 0; i < array1.Length; i++)
     {
-        array1[i]= new Random().Next(minBorder,maxBorder);
+        array1[i]= new Random().NextDouble() * (maxBorder - minBorder) + minBorder;
     }
     return array1;
 }
-int [] userArray = GetRandomArray(arrayL, minBorder, maxBorder);
+double [] userArray = GetRandomArray(arrayL, minBorder, maxBorder);
 
-void PrintUserArray(int [] userArray)
+void PrintUserArray(double [] userArray)
 {
     Console.Write($"Ваш массив: [");
     for (int i = 0; i < (userArray.Length - 1); i++)
@@ -35,7 +35,7 @@ void PrintUserArray(int [] userArray)
     Console.Write($" ]");
 }
 
-int SortOfMinMaxAndCountOfDifference(int[] array)
+double SortOfMinMaxAndCountOfDifference(double[] array)
 {
   for (int i = 0; i < array.Length - 1 ; i++)
   {
@@ -45,16 +45,16 @@ int SortOfMinMaxAndCountOfDifference(int[] array)
     {
         if(array[j] < array[minPosition]) minPosition = j; 
     }
-    int temporary = array[i];
+    double temporary = array[i];
     array[i] = array[minPosition];
     array[minPosition] = temporary;
   }
-  int difference = array[array.Length - 1] - array [0];
+  double difference = array[array.Length - 1] - array [0];
   return difference;
 }
-int differenceOfMaxMinElements = SortOfMinMaxAndCountOfDifference(userArray);
+double differenceOfMaxMinElements = SortOfMinMaxAndCountOfDifference(userArray);
 
-void PrintDifference(int difference)
+void PrintDifference(double difference)
 {
     Console.WriteLine("");
     Console.WriteLine($"Разница между максимальным и минимальным элементом в нём равна {difference}");
