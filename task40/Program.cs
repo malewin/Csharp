9c2749e4e0f33368a6b829f85087f9f565be00df
malewin <139165145+malewin@users.noreply.github.com>
2023-09-07 14:57:38 +0900

    exelent solution

---
+++ task40/Program.cs
@@ -0,0 +1,43 @@
+﻿/* 
+Напишите программу, которая принимает на вход три числа и проверяет,
+может ли существовать треугольник со сторонами такой длины.
+*/
+
+
+int [] FillUserArray()
+{
+    int [] array = new int [3];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int [] arrayOfTriangle = FillUserArray();
+
+bool AnswerTriangle(int [] array)
+{
+    bool answer = false;
+    if(array[0]+array[1] > array[2] 
+    && array[1]+array[2] > array[0]
+    && array[2]+array[0] > array[1])
+    {
+        answer = true;
+    }
+    return answer;
+}
+
+bool answer = AnswerTriangle(arrayOfTriangle)
+
+void PrintAnswer(bool answer)
+{
+    if (answer = true)
+    {
+        Console.WriteLine("да, треугольник с такими сторонами существует");
+    }
+    else
+    {
+        Console.WriteLine("нет, из этих сторон нельзя сложить треугольник");
+    }
+
+}
