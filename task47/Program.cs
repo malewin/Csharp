f9686edde8b642436b2d3ee08fada49ee8f72e09
malewin <139165145+malewin@users.noreply.github.com>
2023-09-14 21:39:02 +0300

    solution is right

---
+++ task47/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задайте двумерный массив размером m×n, заполненный 
+случайными вещественными числами.
+*/
+
+double[,] GetRandom2DArray()
+{
+    Console.Write("Введите количество столбцов: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[rows, cols];
+    Console.Write("Введите минимальное значение: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * (max - min) + min;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double [,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double [,] randomArray = GetRandom2DArray();
+Print2DArray(randomArray);
