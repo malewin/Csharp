65b75460a5269178da27ed7a012a423f63ba586e
malewin <139165145+malewin@users.noreply.github.com>
2023-09-14 09:02:34 +0300

    solution is right

---
+++ task46/Program.cs
@@ -0,0 +1,39 @@
+﻿/* 
+Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+*/
+
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+int[,] result = new int[rowNumber, colNumber];
+for (int i = 0; i < rowNumber; i++)
+{
+for (int j = 0; j < colNumber; j++)
+{
+result[i,j] = new Random().Next(-deviation, deviation);
+}
+}
+return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int [,] randomArray = GetRandom2DArray(5, 5, 10);
+Print2DArray(randomArray);
