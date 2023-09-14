5b88938d6978d2d134067bf996cbe770418b017f
malewin <139165145+malewin@users.noreply.github.com>
2023-09-14 09:14:18 +0300

    solution is right

---
+++ task48/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задайте двумерный массив размера m на n, каждый элемент в массиве 
+находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+*/
+
+int[,] GetRandom2DArray(int m, int n)
+{
+int[,] result = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+result[i,j] = i + j;
+}
+}
+return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int [,] arr = GetRandom2DArray(6,6);
+Print2DArray(arr);
+
