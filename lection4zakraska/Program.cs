6b72c6aa5cc3ee0d58220b065b257ca727f581af
malewin <139165145+malewin@users.noreply.github.com>
2023-09-16 07:07:00 +0300

    solution is right2

--- lection4zakraska/Program.cs
+++ lection4zakraska/Program.cs
@@ -41,3 +41,18 @@ void PrintImage(int[,] image)
 }
 
 PrintImage(pic);
+
+void FillImage(int row, int col)
+{
+  if(pic[row,col] == 0)
+  {
+    pic[row, col] = 1;
+    FillImage(row-1, col);
+    FillImage(row, col-1);
+    FillImage(row+1, col);
+    FillImage(row, col+1);
+  }
+}
+
+FillImage(13, 13);
+PrintImage(pic);
