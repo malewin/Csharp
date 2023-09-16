1436bc8628f871efb44c810553ef12b952c8e033
malewin <139165145+malewin@users.noreply.github.com>
2023-09-16 06:41:02 +0300

    printMethod

--- lection4zakraska/Program.cs
+++ lection4zakraska/Program.cs
@@ -27,3 +27,17 @@ int[,] pic = new int[,]
 
 }
 
+void PrintImage()
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+          if(image[i,j] == 0)  Console.Write($" ");
+          else Console.Write($"+");
+          
+        }
+    }
+}
+
+Print
