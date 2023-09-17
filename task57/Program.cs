def96c8e6e17b3e2f0566dc915202a2bf572ed6d
malewin <139165145+malewin@users.noreply.github.com>
2023-09-17 09:01:26 +0300

    mini-solution is completed

---
+++ task57/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз 
+встречается элемент входных данных.
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раза
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+*/
+
+int[,] Get2DArray()
+{
+    Console.Write("Введите количество столбцов: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимальное значение: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// Разворот матрицы в одномерный массив
+int[] ExpandMatrix(int[,] matrix)
+{
+int n = matrix.GetLength(0) * matrix.GetLength(1);
+int[] array = new int[n];
+n = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+array[n] = matrix[i, j];
+n++;
+}
+}
+return array;
+}
+
+
+void Searcher(int [] array)
+{
+    Console.WriteLine("Какое число ищем в массиве?: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == userNumber) count++;
+    }
+    Console.WriteLine($"Число {userNumber} встречается в массиве {count} раз");
+}
+
+int [,] array2D = Get2DArray();
+Print2DArray(array2D);
+Searcher(ExpandMatrix(array2D));
+
+
