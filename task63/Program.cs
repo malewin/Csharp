f6834543c597cd8c6fce03482975fc9ef9924df2
malewin <139165145+malewin@users.noreply.github.com>
2023-09-19 20:39:22 +0300

    solution is right

---
+++ task63/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от 1 до N.
+*/
+
+void NaturalNumb (int n, int start = 1)
+{
+   if (start <= n)
+   {
+    Console.Write(start + ", ");
+    NaturalNumb(n, start + 1);
+   }
+}
+
+NaturalNumb(5);
