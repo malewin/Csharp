b2b81a62b4cc6248ee276e1a2f191b00b7e64780
malewin <139165145+malewin@users.noreply.github.com>
2023-09-20 00:43:44 +0300

    solution is right

---
+++ task66/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт 
+сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumOfSymbolsBetweenNumbers(int m, int n)
+{
+    int sum = 0;
+    if (m <= n)
+    {
+        sum = m + SumOfSymbolsBetweenNumbers(m + 1, n);
+    }
+    return sum;
+}
+
+int result = SumOfSymbolsBetweenNumbers(m, n);
+Console.WriteLine(result);
+
