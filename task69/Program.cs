c896bc4f23220061c20f79c5e53f775343981de5
malewin <139165145+malewin@users.noreply.github.com>
2023-09-19 22:11:58 +0300

    solution is right

---
+++ task69/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+69. Напишите программу, которая на вход принимает два числа A и B,
+ и возводит число А в целую степень B с помощью рекурсии.
+*/
+
+
+
+
+int SumOfSymbolsInNumber (int numberA, int numberB)
+{
+   if (numberB == 0)
+   return 1;
+   else
+   return numberA * SumOfSymbolsInNumber(numberA, numberB-1);
+}
+
+int result = SumOfSymbolsInNumber(2, 10);
+Console.WriteLine(result);
