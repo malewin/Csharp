e289b33e111fe2267fe00685f0eee7da655264a9
malewin <139165145+malewin@users.noreply.github.com>
2023-09-19 20:54:48 +0300

    solution is right

---
+++ task65/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+65. Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+*/
+
+void NaturalNumb (int m, int n)
+{
+   if (m <= n)
+   {
+    Console.Write(m+ ", ");
+    NaturalNumb(m + 1, n);
+   }
+}
+
+NaturalNumb(1, 10);
