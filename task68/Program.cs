b398a42f0515fd5a552f8b9dbdc91344bc794579
malewin <139165145+malewin@users.noreply.github.com>
2023-09-20 00:53:48 +0300

    solution is right

---
+++ task68/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите число first: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число second: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+int AkkermanFunction(int first, int second)
+ {
+     int answer = 0;
+     if (first != 0)
+     {
+         if (second != 0)
+         {
+             answer = AkkermanFunction(first-1, AkkermanFunction(first, second -1));
+         }
+         else answer = AkkermanFunction(first - 1, 1);
+     }
+     else answer = second + 1; 
+     return answer;
+ }
+
+ Console.Write(AkkermanFunction(first, second));
