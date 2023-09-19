65174edb04e81abc4d2ff99e8c98c205686b4434
malewin <139165145+malewin@users.noreply.github.com>
2023-09-19 21:51:17 +0300

    solution is right

---
+++ task67/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+67. Напишите программу, которая будет принимать 
+на вход число и возвращать сумму его цифр.
+*/
+
+int SumOfSymbolsInNumber (int number)
+{
+   if (number == 0)
+   return 0;
+   else
+   return number % 10 + SumOfSymbolsInNumber(number/10);
+}
+
+int result = SumOfSymbolsInNumber(1234);
+Console.WriteLine(result);
